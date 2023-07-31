b51f9cd0f6a9fe3a080a61944a11f9c01eac516d
zhenghao <zhengh513@outlook.com>
2023-07-31 16:28:30 +0800

    feat: 获取通知数据

---
+++ api/announce.ts
@@ -0,0 +1,28 @@
+export interface AnnounceResult {
+  retCode: number;
+  obj: {
+    bindType: number;
+    content: string;
+    title: string;
+  };
+  msg: {
+    businessCode: number;
+    prompt: string;
+    error: string;
+  };
+}
+
+export function queryAnnounce(bindType: number) {
+  return useFetch('https://cr.zjzs.net/api/common/option/get-system-use', {
+    method: 'POST',
+    body: {
+      args: {
+        bindType,
+      },
+      token: 'dreamtouch',
+      deviceInfo:
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.362023-07-31 15:30:03',
+      channel: 'pc_h5_ksd',
+    },
+  });
+}
--- pages/policy.vue
+++ pages/policy.vue
@@ -1,10 +1,23 @@
 <template>
   <NuxtLayout>
-    <template #title></template>
-    <template #content></template>
+    <template #title>{{ title }}</template>
+    <template #content>
+      <div v-html="content"></div>
+    </template>
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { queryAnnounce } from '~/api/announce';
+import type { AnnounceResult } from '~/api/announce';
+
+const title = ref('');
+const content = ref('');
+onMounted(async () => {
+  const { data } = await queryAnnounce(1);
+  title.value = (data.value as AnnounceResult).obj.title;
+  content.value = (data.value as AnnounceResult).obj.content;
+});
+</script>
 
 <style scoped></style>
