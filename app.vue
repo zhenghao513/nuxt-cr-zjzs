455c80acfa62bc4515969ec653a4e6203ee9b36e
zhenghao <zhengh513@outlook.com>
2023-08-01 13:58:31 +0800

    chore: 全局化中文文案配置

--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <AppHeader />
-    <AppMenu />
-    <NuxtPage />
+    <a-config-provider :locale="zhCN">
+      <AppHeader />
+      <AppMenu />
+      <NuxtPage />
+    </a-config-provider>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
 </script>
 
 <style scoped></style>
