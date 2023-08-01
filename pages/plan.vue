61b7ae46db2a415828d7bb0ea71ca0a9712385d5
zhenghao <zhengh513@outlook.com>
2023-08-01 13:20:23 +0800

    feat(plan): 关键词搜索

--- pages/plan.vue
+++ pages/plan.vue
@@ -2,7 +2,11 @@
   <NuxtLayout name="table">
     <template #title>招生计划查询</template>
     <template #checkbox>
-      <PlanCheckbox />
+      <PlanCheckbox
+        @hierarchical-code-checked-list-change="onHierarchicalChange"
+        @learning-formal-code-checked-list-change="onLearningFormalChange"
+        @is-remedy-change="onisRemedyChange"
+      />
     </template>
     <template #search>
       <PlanSearchInput
@@ -18,14 +22,43 @@
 
 <script setup lang="ts">
 import type { DataSourceType } from '~/components/PlanTable.vue';
+import type { SpecialtyInfoParam, SpecialtyInfoResult } from '~/api/plan';
+import { querySpecialtyInfo } from '~/api/plan';
 
-const { loading, setLoading, toggle } = useLoading();
-const search = ref('');
+onMounted(async () => {
+  await listSpecialtyInfo();
+});
+
+const { loading, setLoading } = useLoading();
 const tableDataSource = ref<DataSourceType[]>([]);
 
-const handleSearchClick = (searchValue: string) => {
-  search.value = searchValue;
-  console.log(search.value);
+const args: SpecialtyInfoParam = {
+  ccdmList: ['1', '2', '3'],
+  keywords: '',
+  pageIndex: 1,
+  pageSize: 10,
+  xxxsdmList: ['1', '2', '4'],
+  hasBbjhs: -1,
+};
+const listSpecialtyInfo = async () => {
+  const { data } = await querySpecialtyInfo(args);
+  tableDataSource.value = (data.value as SpecialtyInfoResult).obj.list;
+};
+
+const onHierarchicalChange = (checkedList: string[]) => {
+  args.ccdmList = checkedList;
+};
+const onLearningFormalChange = (checkedList: string[]) => {
+  args.xxxsdmList = checkedList;
+};
+const onisRemedyChange = (isRemedy: number) => {
+  args.hasBbjhs = isRemedy;
+};
+const handleSearchClick = async (searchValue: string) => {
+  args.keywords = searchValue;
+  setLoading(true);
+  await listSpecialtyInfo();
+  setLoading(false);
 };
 </script>
 
