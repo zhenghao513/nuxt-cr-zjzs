5ca04941dc9313e6bfb397d6391c117b20b9058e
zhenghao <zhengh513@outlook.com>
2023-08-27 21:57:46 +0800

    feat: 出现异常时的提示信息

--- composables/useAnnounce.ts
+++ composables/useAnnounce.ts
@@ -1,13 +1,24 @@
-import type { AnnounceRes } from '~/api/announce';
+import { notification } from 'ant-design-vue';
 
 export default function useAnnounce(bindType: number) {
+  const openNotification = () => {
+    notification.open({
+      message: '没有足够的权限访问该资源',
+      description: '服务器端有能力处理该请求，但是拒绝授权访问。',
+    });
+  };
+
   const title = ref('');
   const content = ref('');
   onMounted(async () => {
     const { data } = await queryAnnounce(bindType);
-    if (data.value?.msg.businessCode === 0) {
-      title.value = data.value.obj.title;
-      content.value = data.value.obj.content;
+    try {
+      if (data.value?.msg.businessCode === 0) {
+        title.value = data.value.obj.title;
+        content.value = data.value.obj.content;
+      }
+    } catch (error) {
+      openNotification();
     }
   });
   return { title, content };
