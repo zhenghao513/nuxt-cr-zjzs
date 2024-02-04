812e9b2197fad2375c94630659f09d92be5d04d9
Zheng Hao <zhengh513@outlook.com>
2024-02-04 15:08:53 +0800

    refactor: 优化请求 body

--- api/notice.ts
+++ api/notice.ts
@@ -15,10 +15,24 @@ export interface NoticeRes {
 export function queryNotice(bindType: number) {
   return useFetch<NoticeRes>('/server/api/common/option/get-system-use', {
     method: 'POST',
-    body: {
+    body: JSON.stringify({
       args: {
         bindType,
       },
-    },
+      token: 'dreamtouch',
+      deviceInfo:
+        navigator.userAgent +
+        new Date()
+          .toLocaleString('zh-CN', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+          })
+          .replaceAll(/\//g, '-'),
+      channel: 'pc_h5_ksd',
+    }),
   });
 }
